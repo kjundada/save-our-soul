15913f4a385066a499b32daa8afabbb354294c00
ZoraizQ <21100130@lums.edu.pk>
2020-07-11 03:36:39 +0500

    add: core video chat server

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { 
-  BrowserRouter as Router, 
-  Route
-} from "react-router-dom";
+import logo from './logo.svg';
 import './App.css';
-
-import MyNavbar from './components/MyNavbar';
-import LandingPage from './components/LandingPage';
-import ConnectedPage from './components/ConnectedPage';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Route path="/" component={ MyNavbar } />
-        <Route exact path="/" component={ LandingPage } />
-        <Route exact path="/connect" component={ ConnectedPage } />
-      </div>
-    </Router>
+    <div className="App">
+      SaveOurSoul
+    </div>
   );
 }
 
