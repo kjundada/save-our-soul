c42e61ceb86a9f90ecb5ed2966a9f6709139e070
ZoraizQ <21100130@lums.edu.pk>
2020-07-12 23:37:59 +0500

    add: createServer

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 'use strict';
-const express = require('express')();
-const http = require('http').Server(express);
-const io = require('socket.io')(http);
+const express = require('express');
+const http = require('http');
 const config = require('./config');
 const chatHandler = require('./chat/handler')();
+const morgan = require('morgan');
+const io = require('socket.io');
+// const path = require('path');
+const PORT = process.env.PORT || config.port;
+// const HOST = process.env.HOST || config.host;
+const app = express();
 
-start();
+app.use(morgan('dev'));
 
-function start() {
-  chatHandler.init(io, express);
-  http.listen(config.port, function() {
-    console.log('Listening on', config.port);
-  });
-}
+const server = http.createServer(app);
+
+
+chatHandler.init(io(server), app);
+
+server.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`) //http://${HOST}:
+});
